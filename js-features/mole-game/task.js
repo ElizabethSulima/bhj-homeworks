aadd38ab5995525c2c2fb06946cb3a95d486103d
Elizabeth <sulimenko_eliz@mail.ru>
2025-10-27 13:10:34 +0300

    homework mole-game done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,45 +1,26 @@
-(() => {
-  let playing = true,
-    activeHole = 1,
-    dead = 0,
-    lost = 0,
-    maxScore = 10,
-    maxLosses = 5;
+let dead = 0,
+  lost = 0,
+  maxScore = 10,
+  maxLosses = 5;
 
-  const deadEl = document.getElementById("dead");
-  const lostEl = document.getElementById("lost");
+const deadEl = document.getElementById("dead");
+const lostEl = document.getElementById("lost");
 
-  const resetStats = () => {
-    (dead = 0), (lost = 0), (deadEl.textContent = dead);
-    lostEl.textContent = lost;
-  };
+const resetStats = () => {
+  (dead = 0), (lost = 0), (deadEl.textContent = dead);
+  lostEl.textContent = lost;
+};
 
-  const getHole = (index) => document.getElementById(`hole${index}`);
-  const deactivateHole = (index) => (getHole(index).className = "hole");
-  const activateHole = (index) =>
-    (getHole(index).className = "hole hole_has-mole");
-
-  const stop = () => (playing = true),
-    next = () =>
-      setTimeout(() => {
-        if (!playing) {
-          return;
-        }
-        deactivateHole(activeHole);
-        activeHole = Math.floor(1 + Math.random() * 9);
-        activateHole(activeHole);
-        next();
-      }, 800);
-
-  const onHoleClick = (idx) => {
-    const holeEl = getHole(idx);
-    const hasMole = holeEl.classList.contains("hole_has-mole");
+const holes = document.querySelectorAll(".hole");
+holes.forEach((hole) => {
+  hole.addEventListener("click", function () {
+    const hasMole = hole.classList.contains("hole_has-mole");
 
     if (hasMole) {
       dead++;
       deadEl.textContent = dead;
 
-      holeEl.classList.remove("hole_has-mole");
+      hole.classList.remove("hole_has-mole");
 
       if (dead >= maxScore) {
         alert("Победа!");
@@ -56,23 +37,13 @@
         return;
       }
     }
-  };
+  });
+});
 
-  const endGame = () => {
-    playing = false;
+const endGame = () => {
+  playing = false;
 
-    for (let i = 1; i <= 9; i++) {
-      deactivateHole(i);
-    }
-    setTimeout(() => {
-      resetStats();
-    }, 0);
-  };
-
-  for (let i = 1; i <= 9; i++) {
-    const holeEl = getHole(i);
-    holeEl.addEventListener("click", () => onHoleClick(i));
-  }
-
-  next();
-})();
+  setTimeout(() => {
+    resetStats();
+  }, 0);
+};
