aa7246385e072852f92ab7a429dfcaa81b3aa7f2
Elizabeth <sulimenko_eliz@mail.ru>
2025-10-29 22:55:04 +0300

    homework tabs add

---
+++ event-object/tabs/task.js
@@ -0,0 +1,46 @@
+function tabNavigations() {
+  const tabNavigations = document.querySelectorAll(".tab__navigation");
+
+  tabNavigations.forEach((nav) => {
+    const contents =
+      nav.nextElementSibling &&
+      nav.nextElementSibling.classList.contains("tab__contents")
+        ? nav.nextElementSibling
+        : null;
+
+    let tabContentGroup = contents;
+    if (!tabContentGroup) {
+      const root = nav.closest(".tab__container") || document.body;
+      tabContentGroup = root.querySelector(".tab__contents");
+    }
+
+    const tabs = [...nav.querySelectorAll(".tab")];
+
+    const contentsItems = tabContentGroup
+      ? [...tabContentGroup.querySelectorAll(".tab__content")]
+      : [];
+
+    tabs.forEach((tab, index) => {
+      tab.addEventListener("click", () => {
+        const activeTab = nav.querySelector(".tab_active");
+        const activeContent = tabContentGroup
+          ? tabContentGroup.querySelector(".tab__content_active")
+          : null;
+
+        if (activeTab) {
+          activeTab.classList.remove("tab_active");
+        }
+        if (activeContent) {
+          activeContent.classList.remove("tab__content_active");
+        }
+
+        tab.classList.add("tab_active");
+        if (contentsItems[index]) {
+          contentsItems[index].classList.add("tab__content_active");
+        }
+      });
+    });
+  });
+}
+
+tabNavigations();
