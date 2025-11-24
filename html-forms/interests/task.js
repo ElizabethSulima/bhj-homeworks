6f7a7e4dc8114303bfec2e3295664af431eaf4cc
Elizabeth <sulimenko_eliz@mail.ru>
2025-11-24 20:42:32 +0300

    homework interest add

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,11 +1,22 @@
-class InterestsManager {
-  constructor(component) {
-    this.component = component;
-    this.interestsCheck = [
-      ...this.component.querySelectorAll(".interest__check"),
-    ];
+function InterestBox() {
+  const parentCheckbox = document.querySelector(".interest__check");
 
+  function setChildCheckboxesState(root, checked) {
+    const childCheckboxes = root.querySelectorAll(
+      'ul.interests input[type="checkbox"]'
+    );
+    childCheckboxes.forEach((box) => {
+      box.checked = checked;
+    });
   }
+
+  parentCheckbox.addEventListener("change", function () {
+    const nestedList =
+      this.closest("li.interest").querySelector("ul.interests");
+    if (nestedList) {
+      setChildCheckboxesState(nestedList, this.checked);
+    }
+  });
 }
 
-const manager = new InterestsManager(document.querySelector(".interests"));
+InterestBox();
