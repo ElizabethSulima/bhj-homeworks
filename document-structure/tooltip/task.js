3586505fff2dc62954e4e7ebd49398e67b443e11
Elizaveta Sulimenko <e.sulimenko@evotor.ru>
2025-12-28 16:25:11 +0300

    dz tooltip add

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,18 +7,29 @@ class TooltipManager {
 
   hintTo() {
     const title = this.component.getAttribute("title");
+
     this.component.addEventListener("mousemove", function (e) {
       this.title = "";
+      this.component.classList.remove("tooltip_active");
     });
+
     this.component.addEventListener("mouseout", function (e) {
       this.title = title;
     });
-    toolthis.componenttip.addEventListener("click", function (event) {
+
+    this.component.addEventListener("click", function (event) {
       event.preventDefault();
 
       const tipDiv = document.createElement("div");
       tipDiv.classList.add("tooltip");
       tipDiv.textContent = title;
+
+      const rect = this.component.getBoundingClientRect();
+      tipDiv.style.position = "fixed";
+      tipDiv.style.top = rect.top + "px";
+      tipDiv.style.left = rect.left + "px";
+      tipDiv.textContent = title;
+
       document.body.appendChild(tipDiv);
     });
   }
